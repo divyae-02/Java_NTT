d36260e914d25e8bb0413fb973f6381f08b77fa6
divyae-02 <61788844+divyae-02@users.noreply.github.com>
2021-11-01 14:10:19 +0530

    Update FlowerNaame.java

--- FlowerNaame.java
+++ FlowerNaame.java
@@ -1,34 +1,21 @@
 package assig1;
-import java.util.Dictionary;
+import java.util.Dictionary;// IMPORTING 
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Scanner;
-public class FlowerNaame {
+public class FlowerNaame 
+{
 	public void displayFlower(int continent)
-	{	
-	}
-	public static void main(String[] args) 
 	{
-		while(true) 
-		{
-			Scanner in=new Scanner(System.in); // TAKING INPUTS
-			System.out.println("Select Any Given Continent to Display Country and National Flowers ");
-			System.out.println("1.ASIA");
-			System.out.println("2.EUROPE");
-			System.out.println("3.AFRICA");
-			System.out.println("4.QUIT");
-			FlowerNaame f1=new FlowerNaame();// CREATING OBJECT
-			int continent=in.nextInt();
-			f1.displayFlower(continent);
-			switch(continent)// SWITCH STATEMENT
+		switch(continent)// SWITCH STATEMENT
 			{
 			case 1:// ASIA
 				System.out.println("------------COUNTRIES IN ASIA-----------");
-				Dictionary<String, String> asia = new Hashtable<>();
+				Dictionary<String, String> asia = new Hashtable<>();//DICTIONARY
 				asia.put("INDIA", "LOTUS");
 			    asia.put("PAKISTHAN", "JASMINE");
 			    asia.put("NEPAL","LALI GURANS");
-			    Enumeration<String> keys=asia.keys();
+			    Enumeration<String> keys=asia.keys();//ITERATING DICT
 			    while(keys.hasMoreElements())
 			    {
 			    	String key=keys.nextElement();
@@ -36,10 +23,10 @@ public class FlowerNaame {
 			    	System.out.println("Country:"+key+" Nationalflower:"+value);
 			    }
 			    System.out.println("----------------------------------------");
-				break;
+			break;
 			case 2:// EUROPE
 				System.out.println("------------EUROPE-----------");
-				Dictionary<String, String> europe = new Hashtable<>();
+				Dictionary<String, String> europe = new Hashtable<>();// DICTIONARY
 				europe.put("GERMANY","CORN FLOWER");
 			    europe.put("FRANCE","LILLY");
 			    europe.put("GREECE","ACANTHUS MOLLIS");
@@ -51,10 +38,10 @@ public class FlowerNaame {
 			    	System.out.println("Country:"+key+" Nationalflower:"+value);
 			    }
 			    System.out.println("----------------------------------------");
-				break;
+			break;
 			case 3:// AFRICA
 				System.out.println("------------COUNTRIES IN AFRICA-----------");
-				Dictionary<String, String> africa = new Hashtable<>();
+				Dictionary<String, String> africa = new Hashtable<>();//DICTIONARY
 		        africa.put("EGYPT","BLUE LOTUS");
 		        africa.put("NIGERIA","YELLOW TRUMPET");
 		        africa.put("KENYA","ORCHID");
@@ -66,12 +53,28 @@ public class FlowerNaame {
 			    	System.out.println("Country:"+key+" NationalFlower:"+value);
 			    }
 			    System.out.println("----------------------------------------");
-				break;
-			case 4:
+			break;
+			case 4:// QUIT
 				System.out.println("--------QUITING-------");
 				System.exit(0);
 			default:System.out.println("None Of the Above");
 			}
+		}	
+	public static void main(String[] args) 
+	{
+		while(true)//LOOPING
+		{
+		Scanner in=new Scanner(System.in); // TAKING INPUTS
+		System.out.println("Select Any Given Continent to Display Country and National Flowers ");
+		System.out.println("1.ASIA");
+		System.out.println("2.EUROPE");
+		System.out.println("3.AFRICA");
+		System.out.println("4.QUIT");
+		int continent=in.nextInt();		
+		FlowerNaame f1=new FlowerNaame(); // CREATING OBJECT
+		f1.displayFlower(continent);
 		}
 	}
 }
+
+
